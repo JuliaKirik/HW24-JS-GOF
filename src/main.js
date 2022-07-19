d7cba4fac01546a63b9b30b997b7514397433574
JuliaKirik <juliakirichok9878@gmail.com>
2022-07-19 12:04:31 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,56 +1,85 @@
-let Jack = (function () {
-    return {
-        registerNotifier: function (notifier) {
-            this.notifier = notifier;
-        },
-        sendMessage: function (msg) {
-            console.log(this.name + " sending message");
-            this.notifier.receiveMessage(this.name, msg);
-        },
-        name: "Jack",
+class PubSub {
+    constructor() {
+        this.handlers = [];
     }
-})();
-
-let Rose = (function () {
-    return {
-        name: "Rose",
-        registerNotifier: function (notifier) {
-            this.notifier = notifier;
-        },
-        receiveMessage: function (messageFrom, message) {
-            console.log(this.name + ': Oh, i got a message from ' + messageFrom);
-            console.log(messageFrom + " : " + message);
-            this.actOnMessage(message)
-        },
-        sendMessage: function (msg) {
-            console.log(this.name + " sending message");
-            this.notifier.receiveMessage(this.name, msg);
-        },
-        actOnMessage (msg) {
-            if (msg === 'I love you') {
-                this.sendMessage('Jack loves me')
+    subscribe(event, handler, context) {
+        if (typeof context === 'undefined') { 
+            context = handler; 
+        }
+        this.handlers.push({ 
+            event: event, 
+            handler: handler.bind(context) 
+        });
+    }
+    publish(event, msg, sender) {
+        this.handlers.forEach((topic) => {
+            if (topic.event === event) {
+                topic.handler(msg, sender)
             }
+        })
+    }
+}
+
+class Rose {
+    constructor(pubsub) {
+        this.name = 'Rose';
+        this.pubsub = pubsub;
+        this.pubsub.subscribe('jackToRose', this.sendMessage, this);
+        this.pubsub.subscribe('billyToRose', this.sendMessage, this);
+    }
+    sendMessage(msg, sender) {
+        console.log(sender + ': ', msg);
+        let receiver;
+        let event;
+        if(sender === 'Jack') {
+            event = 'roseToBilly';
+            receiver = 'Billy'
+        } else {
+            event = 'roseToJack'
+            receiver = 'Jack'
+        }
+        this.publishMessage(event, `${receiver}, ${sender} loves me`);
+    }
+    publishMessage(event, msg) {
+        this.pubsub.publish(event, msg, this.name);
+    }
+}
+
+class Billy {
+    constructor(pubsub) {
+        this.name = 'Billy';
+        this.pubsub = pubsub;
+        this.pubsub.subscribe('roseToBilly', this.sendMessage, this);
+    }
+    sendMessage(msg, sender) {
+        console.log(sender + ': ' + msg);
+        console.log(`${this.name} run`);
+    }
+    publishMessage(event, msg) {
+        this.pubsub.publish(event, msg, this.name);
+    }
+}
+
+class Jack {
+    constructor(pubsub) {
+        this.name = 'Jack';
+        this.pubsub = pubsub;
+        this.pubsub.subscribe('roseToJack', this.sendMessage, this);
+    }
+    sendMessage(msg, sender) {
+        console.log(sender + ': ' + msg);
+        if (sender === 'Rose') {
+            console.log(`${this.name} run`);
         }
     }
-})();
+    publishMessage(event, msg) {
+        this.pubsub.publish(event, msg, this.name);
+    }
+}
 
-let Billy = (function () {
-   return {
-       name: "Billy",
-       receiveMessage: function (messageFrom, message) {
-           console.log(this.name + ': Oh, i got a message from ' + messageFrom);
-           console.log(messageFrom + " : " + message);
-           this.actOnMessage(message)
-       },
-       actOnMessage (msg) {
-           if (msg === 'Jack loves me') {
-               console.log(this.name + ': Run')
-           }
-       }
-   }
-})();
+const pubsub = new PubSub();
+const rose = new Rose(pubsub);
+const billy = new Billy(pubsub);
+const jack = new Jack(pubsub);
 
-Jack.registerNotifier(Rose);
-Rose.registerNotifier(Billy);
-Jack.sendMessage('I love you');
-// Rose.sendMessage('Jack loves me');
+jack.publishMessage('billyToRose', 'Rose I love you!');
